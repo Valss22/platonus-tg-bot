f97cc3c6894b49ee1d25d31138bdc4ea734312bd
Valss22 <valsshokorov@gmail.com>
2022-04-13 11:01:14 +0600

    add structure

--- src/main.py
+++ src/main.py
@@ -1,49 +1,103 @@
-import time
-from selenium import webdriver
-from selenium.webdriver.remote.webelement import WebElement
-from webdriver_manager.chrome import ChromeDriverManager
+import telebot
 
-token = '5115413693:AAFlHsjG9D64RJkhN5JlUgBnT3bKwZTU4o8'
+from src.platonus_selenium.check_marks import token
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
-url = "https://platonus.kgu.kz/"
-driver.get(url)
+a = 0
 
-login_button = driver.find_element_by_id("Submit1")
-login = driver.find_element_by_id('login_input')
-password = driver.find_element_by_id('pass_input')
-
-login.send_keys('Шокоров_Владислав')
-password.send_keys('8222')
-login_button.click()
-time.sleep(1)
-
-# Журнал
-
-driver.find_element_by_xpath('//*[@id="sidebar-nav"]/plt-main-menu/ul/li[3]/a').click()
-time.sleep(1)
-
-# Выбор семестра
-driver.find_element_by_xpath('//*[@id="s2id_autogen2"]/a/span[2]').click()
-time.sleep(1)
-
-# Семестр
-driver.find_element_by_xpath('//*[@id="student_register"]/div/div/div/div/div[2]/div/select/option[2]').click()
-time.sleep(1)
-
-# Итерирование по боксам с предметами
-subjects_marks: list[WebElement] = []
-
-for i in range(1, 20):
-    try:
-        subjects_marks.append(
-            driver.find_element_by_xpath(
-                f'//*[@id="student_register"]/div/div/div/div/div[{str(i)}]/div/div')
-        )
-    except:
-        continue
+bot = telebot.TeleBot(token)
 
 
-print(subjects_marks[0])
-print(len(subjects_marks))
-print(*subjects_marks)
+def inline():
+    inl_key = telebot.types.InlineKeyboardMarkup()
+    but1 = telebot.types.InlineKeyboardButton(text="1", callback_data="test")
+    #   inl_key.add(but1)
+
+    but2 = telebot.types.InlineKeyboardButton(text="2", callback_data="asa")
+    inl_key.add(but1, but2)
+    return inl_key
+
+
+def qwerty():
+    markup1 = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+    markup1.add('Yes', 'No')
+    return markup1
+
+
+def asdfgh():
+    markup2 = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+    markup2.add('Timetable', 'Platonus')
+    return markup2
+
+
+@bot.callback_query_handler(func=lambda c: True)
+def inline_net(c):
+    if c.data == "test":
+        bot.edit_message_text(text="Ok",
+                              chat_id=c.message.chat.id,
+                              message_id=c.message.message_id,
+                              parse_mode='Markdown')
+    elif c.data == "asa":
+        bot.edit_message_text(text="None",
+                              chat_id=c.message.chat.id,
+                              message_id=c.message.message_id,
+                              parse_mode='Markdown')
+
+
+@bot.message_handler(commands=['start', 'go'])
+def rowsing(message):
+    print(str(message.chat.id))
+    markup = telebot.types.ReplyKeyboardMarkup(one_time_keyboard=True, resize_keyboard=True)
+    markup.add('Timetable', 'Platonus')
+    message = bot.reply_to(message, 'Выберите', reply_markup=markup)
+    # bot.register_next_step_handler(message, process_step)
+    # rows.row("Журнал оценок")
+    # rows.row("Расписание")
+
+
+@bot.message_handler(content_types=["text"])
+def row(message):
+    global a
+    global Log
+    global Prl
+    global Sms
+    if message.text == "pil":
+        bot.send_message(message.chat.id, 'qwertyasdfghjklzxcvbnm,.sdfghjkl', reply_markup=inline())
+    if message.text == "Platonus":
+        try:
+            v = parser("Қабдолла_Аңсаған", "1106", "2", "2019")
+        except:
+            print(12)
+            v = parser("Қабдолла_Аңсаған", "1106", "2", "2019")
+        # img = open('{}.PNG'.format(Log+Prl+"1"), 'rb')
+        bot.send_message(message.chat.id, v)
+        # a=0
+        # bot.send_message(message.chat.id,"Введите логин")
+        # a+=1
+    elif a == 1:
+        Log = message.text
+        bot.send_message(message.chat.id, "Введите пароль")
+        a += 1
+    elif a == 2:
+        Prl = message.text
+        bot.send_message(message.chat.id, "Семестр")
+        a += 1
+    elif a == 3:
+        Sms = message.text
+        bot.send_message(message.chat.id, "Ваш логин: {}\nВаш пароль: {}\nСеместр: {}".format(Log, Prl, Sms),
+                         reply_markup=qwerty())
+        a += 1
+    # message= bot.reply_to(message, 'Выберите', reply_markup=qwerty())
+    elif a == 4 and message.text == "Yes":
+        v = parser(Log, Prl, Sms, '2019')
+        img = open('{}.PNG'.format(Log + Prl + "1"), 'rb')
+        bot.send_photo(message.chat.id, img)
+        bot.send_message(message.chat.id, v)
+        a = 0
+    elif a == 4 and message.text == "No":
+        bot.send_message(message.chat.id, "Попробуйте сначала", reply_markup=asdfgh())
+
+        a = 0
+
+
+if __name__ == '__main__':
+    bot.polling(none_stop=True)
